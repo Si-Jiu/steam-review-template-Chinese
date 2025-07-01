3e14f9801b013365c0156660e0c1a1db4be23b16
Si_Jiu49 <si_jiu49@outlook.com>
2025-07-01 13:41:45 +0800

    changed domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://si-jiu.github.io",
-  base: "/steam-review-template-Chinese",
+  site: "https://steam-review.sijiu49.us.kg",
+  base: "/",
   integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: "zh",
